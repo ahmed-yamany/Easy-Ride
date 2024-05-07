669472410eb940f177bc39762edeebc6b514650b
Ahmed Mohammed Yamany <yamany2862016@gmail.com>
2024-05-07 22:21:59 +0300

    Initial Commit

---
+++ EasyRider/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EasyRider
+//
+//  Created by Ahmed Yamany on 07/05/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
