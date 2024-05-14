19c005d99ad949bac22d8a0593c2f668b25ed61a
Ahmed Mohammed Yamany <yamany2862016@gmail.com>
2024-05-14 18:20:28 +0300

    update swiftgen to work with color

--- EasyRider/Scenes/Splash/Presentation/SplashView.swift
+++ EasyRider/Scenes/Splash/Presentation/SplashView.swift
@@ -14,5 +14,6 @@ struct SplashView<ViewModel: SplashViewModelProtocol>: View {
         VStack {
             Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
         }
+        .background(Asset.Colors.erBackground.swiftUIColor)
     }
 }
